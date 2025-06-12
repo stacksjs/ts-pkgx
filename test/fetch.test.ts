d03ac909d2ca234a6762ca12814f95ce992c5be9
Chris <chrisbreuer93@gmail.com>
2025-06-12 15:16:57 -0700

    chore: wip

--- test/fetch.test.ts
+++ test/fetch.test.ts
@@ -3,7 +3,7 @@ import { afterEach, beforeEach, describe, expect, test } from 'bun:test'
 import fs from 'node:fs'
 import os from 'node:os'
 import path from 'node:path'
-import { cleanupBrowserResources, getValidCachedPackage, savePackageAsTypeScript, saveToCacheAndOutput } from '../src/fetch'
+import { cleanupBrowserResources, getValidCachedPackage, savePackageAsTypeScript, saveToCacheAndOutput } from 'ts-pkgx'
 
 // Mock package data for testing
 const mockPackageInfo: PkgxPackage = {
