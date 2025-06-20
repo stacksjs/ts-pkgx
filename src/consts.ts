acabd60974c8fb248296db8475d80096fb2afaef
chrisbbreuer <chrisbreuer93@gmail.com>
2025-06-20 02:36:35 +0000

    chore: update pantry data (1 files)
    Automated pantry update via GitHub Actions workflow

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 /**
  * Constants used throughout the ts-pkgx package
  * This file is auto-generated from pantry. Do not edit manually.
- * Generated on 2025-06-19T16:26:41.940Z
+ * Generated on 2025-06-20T02:36:35.227Z
  */
 
 /**
