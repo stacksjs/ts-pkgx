8f79c646e654f904ddd8fdec47af63986612a4bc
chrisbbreuer <chrisbreuer93@gmail.com>
2025-06-28 17:26:53 +0000

    chore: update crates.io_spider_cli, github.com_ggerganov_llama.cpp, vimo
    Automated update via GitHub Actions workflow

--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.1485` (2057 versions available)
+ * @version `9.1.1489` (2061 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "vim.org"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.1485" (latest)
+ * console.log(pkg.versions[0]) // "9.1.1489" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -74,6 +74,10 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.1489',
+    '9.1.1488',
+    '9.1.1487',
+    '9.1.1486',
     '9.1.1485',
     '9.1.1484',
     '9.1.1483',
