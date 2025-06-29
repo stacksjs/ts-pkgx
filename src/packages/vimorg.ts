78222d0df505b439f18cced6ec8fc0ebc322a8a7
chrisbbreuer <chrisbreuer93@gmail.com>
2025-06-29 15:29:13 +0000

    chore: update astral.sh_uv, vimorg
    Automated update via GitHub Actions workflow

--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.1492` (2064 versions available)
+ * @version `9.1.1493` (2065 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "vim.org"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.1492" (latest)
+ * console.log(pkg.versions[0]) // "9.1.1493" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -74,6 +74,7 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.1493',
     '9.1.1492',
     '9.1.1491',
     '9.1.1490',
