11f989115df5ceb04e66c58720d9faf6c436942a
chrisbbreuer <chrisbreuer93@gmail.com>
2025-07-11 01:59:26 +0000

    chore: update bittensorcom, github.com_ggerganov_llama.cpp
    Automated update via GitHub Actions workflow

--- src/packages/bittensorcom.ts
+++ src/packages/bittensorcom.ts
@@ -2,7 +2,7 @@
  * **Bittensor** - Internet-scale Neural Networks
  *
  * @domain `bittensor.com`
- * @version `9.8.1` (65 versions available)
+ * @version `9.8.2` (66 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install Bittensor`
@@ -20,7 +20,7 @@
  * console.log(pkg === samePkg) // true
  * console.log(pkg.name)        // "Bittensor"
  * console.log(pkg.description) // "Internet-scale Neural Networks"
- * console.log(pkg.versions[0]) // "9.8.1" (latest)
+ * console.log(pkg.versions[0]) // "9.8.2" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/bittensor-com.md
@@ -62,6 +62,7 @@ export const bittensorPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.8.2',
     '9.8.1',
     '9.8.0',
     '9.7.0',
