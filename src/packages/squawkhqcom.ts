413b4ebfff8e75ab6e25c03635a54d19aa8c2023
chrisbbreuer <chrisbreuer93@gmail.com>
2025-07-12 20:30:37 +0000

    chore: update crates.io_eza, squawkhqcom
    Automated update via GitHub Actions workflow

--- src/packages/squawkhqcom.ts
+++ src/packages/squawkhqcom.ts
@@ -3,7 +3,7 @@
  *
  * @domain `squawkhq.com`
  * @programs `squawk`
- * @version `2.19.0` (42 versions available)
+ * @version `2.20.0` (43 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install squawk`
@@ -21,7 +21,7 @@
  * console.log(pkg.name)        // "squawk"
  * console.log(pkg.description) // "🐘 linter for PostgreSQL, focused on migrations"
  * console.log(pkg.programs)    // ["squawk"]
- * console.log(pkg.versions[0]) // "2.19.0" (latest)
+ * console.log(pkg.versions[0]) // "2.20.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/squawkhq-com.md
@@ -62,6 +62,7 @@ export const squawkPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '2.20.0',
     '2.19.0',
     '2.18.0',
     '2.17.0',
