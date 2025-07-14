cc3c12a4a6a81985eb0375ea7b1bbaa6fb715971
chrisbbreuer <chrisbreuer93@gmail.com>
2025-07-14 19:29:29 +0000

    chore: update astral.sh_uv and 8 other deps
    Automated update via GitHub Actions workflow

--- src/packages/tartrun.ts
+++ src/packages/tartrun.ts
@@ -3,7 +3,7 @@
  *
  * @domain `tart.run`
  * @programs `tart`
- * @version `2.28.1` (57 versions available)
+ * @version `2.28.2` (58 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install tart`
@@ -21,7 +21,7 @@
  * console.log(pkg.name)        // "tart"
  * console.log(pkg.description) // "macOS and Linux VMs on Apple Silicon to use in ..."
  * console.log(pkg.programs)    // ["tart"]
- * console.log(pkg.versions[0]) // "2.28.1" (latest)
+ * console.log(pkg.versions[0]) // "2.28.2" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/tart-run.md
@@ -62,6 +62,7 @@ export const tartPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '2.28.2',
     '2.28.1',
     '2.28.0',
     '2.27.3',
