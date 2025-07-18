08f017f7862926e1f3e55fbc49b01be75b0fe43b
chrisbbreuer <chrisbreuer93@gmail.com>
2025-07-18 20:30:11 +0000

    chore: update aws.amazon.com_cli, daggerio and 3 other deps
    Automated update via GitHub Actions workflow

--- src/packages/aws.amazon.com/cli.ts
+++ src/packages/aws.amazon.com/cli.ts
@@ -3,7 +3,7 @@
  *
  * @domain `aws.amazon.com/cli`
  * @programs `aws`
- * @version `2.27.54` (957 versions available)
+ * @version `2.27.55` (959 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install aws`
@@ -23,7 +23,7 @@
  * console.log(pkg.name)        // "aws/cli"
  * console.log(pkg.description) // "Universal Command Line Interface for Amazon Web..."
  * console.log(pkg.programs)    // ["aws"]
- * console.log(pkg.versions[0]) // "2.27.54" (latest)
+ * console.log(pkg.versions[0]) // "2.27.55" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/aws-amazon-com/cli.md
@@ -71,6 +71,7 @@ export const awsPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '2.27.55',
     '2.27.54',
     '2.27.53',
     '2.27.52',
@@ -477,6 +478,7 @@ export const awsPackage = {
     '2.11.7',
     '2.10.4',
     '2.10.3',
+    '1.41.9',
     '1.41.8',
     '1.41.7',
     '1.41.6',
