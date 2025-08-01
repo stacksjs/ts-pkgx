f24aabb1bbcb7ca488fd44b18a8b9d6fa31eb65c
chrisbbreuer <chrisbreuer93@gmail.com>
2025-08-01 17:32:17 +0000

    chore: update github.com_anchore_grype, opencodeai
    Automated update via GitHub Actions workflow

--- docs/package-catalog.md
+++ docs/package-catalog.md
@@ -8,7 +8,7 @@ Each package can be accessed using `getPackage(name)` or directly via `pantry.do
 
 - **Total Packages**: 1397
 - **Categories**: 27
-- **Last Updated**: 2025-08-01T16:31:06.367Z
+- **Last Updated**: 2025-08-01T17:32:16.348Z
 
 ## Table of Contents
 
--- src/packages/github.com/anchore/grype.ts
+++ src/packages/github.com/anchore/grype.ts
@@ -3,7 +3,7 @@
  *
  * @domain `github.com/anchore/grype`
  * @programs `grype`
- * @version `0.97.0` (59 versions available)
+ * @version `0.97.1` (60 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install grype`
@@ -21,7 +21,7 @@
  * console.log(pkg.name)        // "grype"
  * console.log(pkg.description) // "A vulnerability scanner for container images an..."
  * console.log(pkg.programs)    // ["grype"]
- * console.log(pkg.versions[0]) // "0.97.0" (latest)
+ * console.log(pkg.versions[0]) // "0.97.1" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/github-com/anchore/grype.md
@@ -62,6 +62,7 @@ export const grypePackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '0.97.1',
     '0.97.0',
     '0.96.1',
     '0.96.0',
