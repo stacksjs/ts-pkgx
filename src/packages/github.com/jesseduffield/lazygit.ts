a3505ff48daff6ca5a8eee2d91329fda8affdf70
chrisbbreuer <chrisbreuer93@gmail.com>
2025-08-02 09:30:22 +0000

    chore: update github.com_ggerganov_llama.cpp, github.com_jesseduffield_l
    Automated update via GitHub Actions workflow

--- src/packages/github.com/jesseduffield/lazygit.ts
+++ src/packages/github.com/jesseduffield/lazygit.ts
@@ -3,7 +3,7 @@
  *
  * @domain `github.com/jesseduffield/lazygit`
  * @programs `lazygit`
- * @version `0.53.1` (29 versions available)
+ * @version `0.54.0` (30 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install lazygit`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "lazygit"
  * console.log(pkg.description) // "simple terminal UI for git commands"
  * console.log(pkg.programs)    // ["lazygit"]
- * console.log(pkg.versions[0]) // "0.53.1" (latest)
+ * console.log(pkg.versions[0]) // "0.54.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/github-com/jesseduffield/lazygit.md
@@ -69,6 +69,7 @@ export const lazygitPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '0.54.0',
     '0.53.1',
     '0.53.0',
     '0.52.0',
