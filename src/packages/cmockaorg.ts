c6c7023150cef685ef3f7b6d1c669c4962f9a163
chrisbbreuer <chrisbreuer93@gmail.com>
2025-08-10 18:30:20 +0000

    chore: update cmockaorg
    Automated update via GitHub Actions workflow

--- src/packages/cmockaorg.ts
+++ src/packages/cmockaorg.ts
@@ -2,8 +2,6 @@
  * **cmocka.org** - pkgx package
  *
  * @domain `cmocka.org`
- * @version `1.1.8` (2 versions available)
- * @versions From newest version to oldest.
  *
  * @install `launchpad install cmocka.org`
  *
@@ -13,7 +11,6 @@
  *
  * const pkg = pantry.cmockaorg
  * console.log(pkg.name)        // "cmocka.org"
- * console.log(pkg.versions[0]) // "1.1.8" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/cmocka-org.md
@@ -32,9 +29,9 @@ export const cmockaorgPackage = {
    * Brief description of what this package does.
    */
   description: '' as const,
-  packageYmlUrl: 'https://github.com/pkgxdev/pantry/tree/main/projects/cmocka.org/package.yml' as const,
+  packageYmlUrl: '' as const,
   homepageUrl: '' as const,
-  githubUrl: 'https://github.com/pkgxdev/pantry/' as const,
+  githubUrl: '' as const,
   /**
    * Command to install this package using launchpad.
    * @example launchpad install package-name
@@ -43,14 +40,7 @@ export const cmockaorgPackage = {
   programs: [] as const,
   companions: [] as const,
   dependencies: [] as const,
-  /**
-   * Available versions from newest to oldest.
-   * @see https://ts-pkgx.netlify.app/usage for installation instructions
-   */
-  versions: [
-    '1.1.8',
-    '1.1.7',
-  ] as const,
+  versions: [] as const,
   aliases: [] as const,
   pkgxInstallCommand: 'sh <(curl https://pkgx.sh) +cmocka.org -- $SHELL -i' as const,
   launchpadInstallCommand: 'launchpad install cmocka.org' as const,
