9300cd7481f5fa769fb0fc5f75fe1d1d1a8c0462
chrisbbreuer <chrisbreuer93@gmail.com>
2025-08-17 07:29:14 +0000

    chore: update ijgorg
    Automated update via GitHub Actions workflow

--- src/packages/ijgorg.ts
+++ src/packages/ijgorg.ts
@@ -2,6 +2,9 @@
  * **ijg.org** - pkgx package
  *
  * @domain `ijg.org`
+ * @programs `cjpeg`, `djpeg`, `jpegtran`, `rdjpgcom`, `wrjpgcom`
+ * @version `9f` (3 versions available)
+ * @versions From newest version to oldest.
  *
  * @install `launchpad install ijg.org`
  *
@@ -11,6 +14,8 @@
  *
  * const pkg = pantry.ijgorg
  * console.log(pkg.name)        // "ijg.org"
+ * console.log(pkg.programs)    // ["cjpeg", "djpeg", ...]
+ * console.log(pkg.versions[0]) // "9f" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/ijg-org.md
@@ -29,18 +34,36 @@ export const ijgorgPackage = {
    * Brief description of what this package does.
    */
   description: '' as const,
-  packageYmlUrl: '' as const,
+  packageYmlUrl: 'https://github.com/pkgxdev/pantry/tree/main/projects/ijg.org/package.yml' as const,
   homepageUrl: '' as const,
-  githubUrl: '' as const,
+  githubUrl: 'https://github.com/pkgxdev/pantry/' as const,
   /**
    * Command to install this package using launchpad.
    * @example launchpad install package-name
    */
   installCommand: 'launchpad install ijg.org' as const,
-  programs: [] as const,
+  /**
+   * Executable programs provided by this package.
+   * These can be run after installation.
+   */
+  programs: [
+    'cjpeg',
+    'djpeg',
+    'jpegtran',
+    'rdjpgcom',
+    'wrjpgcom',
+  ] as const,
   companions: [] as const,
   dependencies: [] as const,
-  versions: [] as const,
+  /**
+   * Available versions from newest to oldest.
+   * @see https://ts-pkgx.netlify.app/usage for installation instructions
+   */
+  versions: [
+    '9f',
+    '9e',
+    '9.5.0',
+  ] as const,
   aliases: [] as const,
   pkgxInstallCommand: 'sh <(curl https://pkgx.sh) +ijg.org -- $SHELL -i' as const,
   launchpadInstallCommand: 'launchpad install ijg.org' as const,
