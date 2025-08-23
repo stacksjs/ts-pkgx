57b4670883a631a1d4c4519345a8a25d191d6b95
chrisbbreuer <chrisbreuer93@gmail.com>
2025-08-23 15:28:50 +0000

    chore: update github.com_ggerganov_llama.cpp, projectdiscovery.io_nuclei
    Automated update via GitHub Actions workflow

--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.1671` (2236 versions available)
+ * @version `9.1.1676` (2240 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "vim.org"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.1671" (latest)
+ * console.log(pkg.versions[0]) // "9.1.1676" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -74,6 +74,10 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.1676',
+    '9.1.1675',
+    '9.1.1674',
+    '9.1.1672',
     '9.1.1671',
     '9.1.1670',
     '9.1.1669',
