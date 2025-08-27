7a3d3929e4ebceb005e65dbf9cc3c4f60420857a
chrisbbreuer <chrisbreuer93@gmail.com>
2025-08-27 09:30:51 +0000

    chore: update browser-usecom, checkovio
    Automated update via GitHub Actions workflow

--- src/packages/browser-usecom.ts
+++ src/packages/browser-usecom.ts
@@ -2,7 +2,7 @@
  * **browser-use.com** - Make websites accessible for AI agents
  *
  * @domain `browser-use.com`
- * @version `0.6.3` (43 versions available)
+ * @version `0.7.0` (44 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install browser-use.com`
@@ -15,7 +15,7 @@
  * const pkg = pantry.browserusecom
  * console.log(pkg.name)        // "browser-use.com"
  * console.log(pkg.description) // "Make websites accessible for AI agents"
- * console.log(pkg.versions[0]) // "0.6.3" (latest)
+ * console.log(pkg.versions[0]) // "0.7.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/browser-use-com.md
@@ -56,6 +56,7 @@ export const browserusecomPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '0.7.0',
     '0.6.3',
     '0.6.2',
     '0.6.1',
