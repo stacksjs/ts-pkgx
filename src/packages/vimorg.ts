67e6a9d42ab728f1e5e5feaad09db74770476274
chrisbbreuer <chrisbreuer93@gmail.com>
2025-08-29 17:28:37 +0000

    chore: update vimorg
    Automated update via GitHub Actions workflow

--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.1710` (2273 versions available)
+ * @version `9.1.1713` (2276 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "vim.org"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.1710" (latest)
+ * console.log(pkg.versions[0]) // "9.1.1713" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -74,6 +74,9 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.1713',
+    '9.1.1712',
+    '9.1.1711',
     '9.1.1710',
     '9.1.1709',
     '9.1.1708',
