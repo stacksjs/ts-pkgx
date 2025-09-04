da0bb55505941e407cbc6829016c17c27f85ba8e
chrisbbreuer <chrisbreuer93@gmail.com>
2025-09-04 12:32:51 +0000

    chore: update github.com_ggerganov_llama.cpp, github.com_hykilpikonna_hy
    Automated update via GitHub Actions workflow

--- docs/packages/github.com/hykilpikonna/hyfetch-neowofetch.md
+++ docs/packages/github.com/hykilpikonna/hyfetch-neowofetch.md
@@ -25,13 +25,13 @@ This package provides the following executable programs:
 ## Available Versions
 
 <details>
-<summary>Show all 2 versions</summary>
+<summary>Show all 3 versions</summary>
 
-- `1.99.0`, `1.4.11`
+- `2.0.2`, `1.99.0`, `1.4.11`
 
 </details>
 
-**Latest Version**: `1.99.0`
+**Latest Version**: `2.0.2`
 
 ### Install Specific Version
 
--- src/packages/github.com/hykilpikonna/hyfetch/neowofetch.ts
+++ src/packages/github.com/hykilpikonna/hyfetch/neowofetch.ts
@@ -3,7 +3,7 @@
  *
  * @domain `github.com/hykilpikonna/hyfetch/neowofetch`
  * @programs `neowofetch`
- * @version `1.99.0` (2 versions available)
+ * @version `2.0.2` (3 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install neowofetch`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "neowofetch"
  * console.log(pkg.description) // "Fast, highly customisable system info script wi..."
  * console.log(pkg.programs)    // ["neowofetch"]
- * console.log(pkg.versions[0]) // "1.99.0" (latest)
+ * console.log(pkg.versions[0]) // "2.0.2" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/github-com/hykilpikonna/hyfetch/neowofetch.md
@@ -69,6 +69,7 @@ export const neowofetchPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '2.0.2',
     '1.99.0',
     '1.4.11',
   ] as const,
