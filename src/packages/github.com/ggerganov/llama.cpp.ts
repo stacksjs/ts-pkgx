cf01220addfbabf831164a84473ae7650fa756d0
chrisbbreuer <chrisbreuer93@gmail.com>
2025-09-14 17:29:30 +0000

    chore: update fliptio, github.com_ggerganov_llama.cpp
    Automated update via GitHub Actions workflow

--- src/packages/github.com/ggerganov/llama.cpp.ts
+++ src/packages/github.com/ggerganov/llama.cpp.ts
@@ -3,7 +3,7 @@
  *
  * @domain `github.com/ggerganov/llama.cpp`
  * @programs `llama-cli`, `llama.cpp`, `convert.py`
- * @version `6469.0.0` (3259 versions available)
+ * @version `6471.0.0` (3261 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install github.com/ggerganov/llama.cpp`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "LLaMA.cpp"
  * console.log(pkg.description) // "LLM inference in C/C++"
  * console.log(pkg.programs)    // ["llama-cli", "llama.cpp", ...]
- * console.log(pkg.versions[0]) // "6469.0.0" (latest)
+ * console.log(pkg.versions[0]) // "6471.0.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/github-com/ggerganov/llama-cpp.md
@@ -74,6 +74,8 @@ export const lLaMAcppPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '6471.0.0',
+    '6470.0.0',
     '6469.0.0',
     '6451.0.0',
     '6447.0.0',
