22d40173cd226db407ddbb6d3b586a5396884dcf
chrisbbreuer <chrisbreuer93@gmail.com>
2025-09-16 20:32:06 +0000

    chore: update aws.amazon.com_cli, bufbuild, flyio, vimorg
    Automated update via GitHub Actions workflow

--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.1765` (2324 versions available)
+ * @version `9.1.1766` (2325 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "vim.org"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.1765" (latest)
+ * console.log(pkg.versions[0]) // "9.1.1766" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -74,6 +74,7 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.1766',
     '9.1.1765',
     '9.1.1764',
     '9.1.1763',
