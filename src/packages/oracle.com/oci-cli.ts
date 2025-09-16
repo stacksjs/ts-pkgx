b17ec3ccd4af16f5b411bc1dedc47a902e3206f8
chrisbbreuer <chrisbreuer93@gmail.com>
2025-09-16 08:31:07 +0000

    chore: update huggingfaceco, lavinmqcom, oracle.com_oci-cli
    Automated update via GitHub Actions workflow

--- src/packages/oracle.com/oci-cli.ts
+++ src/packages/oracle.com/oci-cli.ts
@@ -3,7 +3,7 @@
  *
  * @domain `oracle.com/oci-cli`
  * @programs `oci`
- * @version `3.66.0` (79 versions available)
+ * @version `3.66.1` (80 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install oci`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "oci"
  * console.log(pkg.description) // "Command Line Interface for Oracle Cloud Infrast..."
  * console.log(pkg.programs)    // ["oci"]
- * console.log(pkg.versions[0]) // "3.66.0" (latest)
+ * console.log(pkg.versions[0]) // "3.66.1" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/oracle-com/oci-cli.md
@@ -72,6 +72,7 @@ export const ociPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '3.66.1',
     '3.66.0',
     '3.65.1',
     '3.65.0',
