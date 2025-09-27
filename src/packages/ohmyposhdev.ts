9569872f8fa2725f2fe0dbb592897a6cbb65b6e7
chrisbbreuer <chrisbreuer93@gmail.com>
2025-09-27 12:31:31 +0000

    chore: update github.com_ggerganov_llama.cpp, ohmyposhdev
    Automated update via GitHub Actions workflow

--- src/packages/ohmyposhdev.ts
+++ src/packages/ohmyposhdev.ts
@@ -3,7 +3,7 @@
  *
  * @domain `ohmyposh.dev`
  * @programs `oh-my-posh`
- * @version `26.25.0` (330 versions available)
+ * @version `26.26.0` (331 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install oh-my-posh`
@@ -21,7 +21,7 @@
  * console.log(pkg.name)        // "oh-my-posh"
  * console.log(pkg.description) // "The most customisable and low-latency cross pla..."
  * console.log(pkg.programs)    // ["oh-my-posh"]
- * console.log(pkg.versions[0]) // "26.25.0" (latest)
+ * console.log(pkg.versions[0]) // "26.26.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/ohmyposh-dev.md
@@ -62,6 +62,7 @@ export const ohmyposhPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '26.26.0',
     '26.25.0',
     '26.24.1',
     '26.24.0',
