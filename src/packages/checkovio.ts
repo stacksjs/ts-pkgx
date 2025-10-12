cadf69e83d3bfeefe142ead183a8ce9a9e1aabb8
chrisbbreuer <chrisbreuer93@gmail.com>
2025-10-12 13:34:23 +0000

    chore: update checkovio
    Automated update via GitHub Actions workflow

--- src/packages/checkovio.ts
+++ src/packages/checkovio.ts
@@ -3,7 +3,7 @@
  *
  * @domain `checkov.io`
  * @programs `checkov`
- * @version `3.2.477` (508 versions available)
+ * @version `3.2.482` (509 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install checkov`
@@ -22,7 +22,7 @@
  * console.log(pkg.name)        // "checkov"
  * console.log(pkg.description) // "Prevent cloud misconfigurations and find vulner..."
  * console.log(pkg.programs)    // ["checkov"]
- * console.log(pkg.versions[0]) // "3.2.477" (latest)
+ * console.log(pkg.versions[0]) // "3.2.482" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/checkov-io.md
@@ -69,6 +69,7 @@ export const checkovPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '3.2.482',
     '3.2.477',
     '3.2.476',
     '3.2.475',
