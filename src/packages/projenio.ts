883b06adca3a5cec27858094c17700ca2420df99
chrisbbreuer <chrisbreuer93@gmail.com>
2025-10-13 14:31:56 +0000

    chore: update digitalocean.com_doctl, github.com_ggerganov_llama.cpp, mi
    Automated update via GitHub Actions workflow

--- src/packages/projenio.ts
+++ src/packages/projenio.ts
@@ -3,7 +3,7 @@
  *
  * @domain `projen.io`
  * @programs `projen`
- * @version `0.97.2` (62 versions available)
+ * @version `0.98.0` (63 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install projen`
@@ -23,7 +23,7 @@
  * console.log(pkg.name)        // "projen"
  * console.log(pkg.description) // "Rapidly build modern applications with advanced..."
  * console.log(pkg.programs)    // ["projen"]
- * console.log(pkg.versions[0]) // "0.97.2" (latest)
+ * console.log(pkg.versions[0]) // "0.98.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/projen-io.md
@@ -80,6 +80,7 @@ export const projenPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '0.98.0',
     '0.97.2',
     '0.97.1',
     '0.97.0',
