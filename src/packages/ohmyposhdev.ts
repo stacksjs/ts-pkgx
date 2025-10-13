5410966620e92bc5ad7a1c2cead4ee21045c2cce
chrisbbreuer <chrisbreuer93@gmail.com>
2025-10-13 06:33:49 +0000

    chore: update ohmyposhdev
    Automated update via GitHub Actions workflow

--- src/packages/ohmyposhdev.ts
+++ src/packages/ohmyposhdev.ts
@@ -3,7 +3,7 @@
  *
  * @domain `ohmyposh.dev`
  * @programs `oh-my-posh`
- * @version `27.1.2` (338 versions available)
+ * @version `27.2.0` (339 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install oh-my-posh`
@@ -21,7 +21,7 @@
  * console.log(pkg.name)        // "oh-my-posh"
  * console.log(pkg.description) // "The most customisable and low-latency cross pla..."
  * console.log(pkg.programs)    // ["oh-my-posh"]
- * console.log(pkg.versions[0]) // "27.1.2" (latest)
+ * console.log(pkg.versions[0]) // "27.2.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/ohmyposh-dev.md
@@ -62,6 +62,7 @@ export const ohmyposhPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '27.2.0',
     '27.1.2',
     '27.1.1',
     '27.1.0',
