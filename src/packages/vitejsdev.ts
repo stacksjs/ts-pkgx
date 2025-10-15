16c8e4d62d37bed3e8c8fd4997b29a351030c8dd
chrisbbreuer <chrisbreuer93@gmail.com>
2025-10-15 10:10:53 +0000

    chore: update squidfunk/mkdocs-material, plakar, vite
    Automated update via GitHub Actions workflow

--- src/packages/vitejsdev.ts
+++ src/packages/vitejsdev.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vitejs.dev`
  * @programs `vite`
- * @version `7.1.10` (122 versions available)
+ * @version `7.1.10` (123 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vitejs.dev`
@@ -96,6 +96,7 @@ export const vitejsdevPackage = {
     '7.0.2',
     '7.0.1',
     '7.0.0',
+    '6.4.0',
     '6.3.7',
     '6.3.6',
     '6.3.5',
