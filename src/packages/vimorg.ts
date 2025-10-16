ec3e8f1c52888e378545e83f5e097890a571006f
chrisbbreuer <chrisbreuer93@gmail.com>
2025-10-16 20:10:46 +0000

    chore: update aws/cli, vim, zrok
    Automated update via GitHub Actions workflow

--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.1862` (2415 versions available)
+ * @version `9.1.1863` (2416 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -25,7 +25,7 @@
  * console.log(pkg.name)        // "vim"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.1862" (latest)
+ * console.log(pkg.versions[0]) // "9.1.1863" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -86,6 +86,7 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.1863',
     '9.1.1862',
     '9.1.1861',
     '9.1.1860',
