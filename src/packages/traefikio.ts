f41c5a3e7048261aa04babc5d59640991f3a1ba3
chrisbbreuer <chrisbreuer93@gmail.com>
2025-10-28 12:13:37 +0000

    chore: update traefik
    Automated update via GitHub Actions workflow

--- src/packages/traefikio.ts
+++ src/packages/traefikio.ts
@@ -3,7 +3,7 @@
  *
  * @domain `traefik.io`
  * @programs `traefik`
- * @version `3.5.3` (71 versions available)
+ * @version `3.5.4` (72 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install traefik.io`
@@ -18,7 +18,7 @@
  * console.log(pkg.name)        // "traefik"
  * console.log(pkg.description) // "The Cloud Native Application Proxy"
  * console.log(pkg.programs)    // ["traefik"]
- * console.log(pkg.versions[0]) // "3.5.3" (latest)
+ * console.log(pkg.versions[0]) // "3.5.4" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/traefik-io.md
@@ -68,6 +68,7 @@ export const traefikioPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '3.5.4',
     '3.5.3',
     '3.5.2',
     '3.5.1',
