bd8e4cdd48348d1882d8072dbc2b7061b435f4fd
chrisbbreuer <chrisbreuer93@gmail.com>
2025-11-02 14:09:38 +0000

    chore: update csvlens, cargo-c, corepack
    Automated update via GitHub Actions workflow

--- src/packages/nodejs.org/corepack.ts
+++ src/packages/nodejs.org/corepack.ts
@@ -3,7 +3,7 @@
  *
  * @domain `nodejs.org/corepack`
  * @programs `corepack`
- * @version `0.34.1` (14 versions available)
+ * @version `0.34.2` (15 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install nodejs.org/corepack`
@@ -19,7 +19,7 @@
  * console.log(pkg.name)        // "corepack"
  * console.log(pkg.description) // "Platform built on V8 to build network applications"
  * console.log(pkg.programs)    // ["corepack"]
- * console.log(pkg.versions[0]) // "0.34.1" (latest)
+ * console.log(pkg.versions[0]) // "0.34.2" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/nodejs-org/corepack.md
@@ -75,6 +75,7 @@ export const nodejsorgcorepackPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '0.34.2',
     '0.34.1',
     '0.34.0',
     '0.33.0',
