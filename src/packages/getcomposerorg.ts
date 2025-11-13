5d49efe5f81353bd956042c78061523dc68ee7ef
chrisbbreuer <chrisbreuer93@gmail.com>
2025-11-13 16:12:27 +0000

    chore: update daytona, composer
    Automated update via GitHub Actions workflow

--- src/packages/getcomposerorg.ts
+++ src/packages/getcomposerorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `getcomposer.org`
  * @programs `composer`, `composer.phar`
- * @version `2.9.0` (34 versions available)
+ * @version `2.9.1` (35 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install getcomposer.org`
@@ -19,7 +19,7 @@
  * console.log(pkg.name)        // "composer"
  * console.log(pkg.description) // "Dependency Manager for PHP"
  * console.log(pkg.programs)    // ["composer", "composer.phar"]
- * console.log(pkg.versions[0]) // "2.9.0" (latest)
+ * console.log(pkg.versions[0]) // "2.9.1" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/getcomposer-org.md
@@ -76,6 +76,7 @@ export const getcomposerorgPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '2.9.1',
     '2.9.0',
     '2.8.12',
     '2.8.11',
