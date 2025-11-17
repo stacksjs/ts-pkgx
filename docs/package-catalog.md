314501598db623eec42ffd7475ea571ee5bd95bb
chrisbbreuer <chrisbreuer93@gmail.com>
2025-11-17 11:11:17 +0000

    chore: update rucio-client
    Automated update via GitHub Actions workflow

--- docs/package-catalog.md
+++ docs/package-catalog.md
@@ -8,7 +8,7 @@ Each package can be accessed using `getPackage(name)` or directly via `pantry.do
 
 - **Total Packages**: 1399
 - **Categories**: 27
-- **Last Updated**: 2025-11-17T10:12:01.090Z
+- **Last Updated**: 2025-11-17T11:11:16.311Z
 
 ## Table of Contents
 
--- src/packages/rucio.cern.ch/rucio-client.ts
+++ src/packages/rucio.cern.ch/rucio-client.ts
@@ -3,7 +3,7 @@
  *
  * @domain `rucio.cern.ch/rucio-client`
  * @programs `rucio`, `rucio-admin`
- * @version `38.5.0` (38 versions available)
+ * @version `38.5.1` (39 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install rucio.cern.ch/rucio-client`
@@ -19,7 +19,7 @@
  * console.log(pkg.name)        // "rucio-client"
  * console.log(pkg.description) // "Rucio - Scientific Data Management"
  * console.log(pkg.programs)    // ["rucio", "rucio-admin"]
- * console.log(pkg.versions[0]) // "38.5.0" (latest)
+ * console.log(pkg.versions[0]) // "38.5.1" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/rucio-cern-ch/rucio-client.md
@@ -78,6 +78,7 @@ export const ruciocernchrucioclientPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '38.5.1',
     '38.5.0',
     '38.4.0',
     '38.3.0',
