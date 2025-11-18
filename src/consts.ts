43395b23c245aedc039fdf67018d7289d0a6367a
chrisbbreuer <chrisbreuer93@gmail.com>
2025-11-18 02:36:40 +0000

    chore: update registry data (9 files)
    Automated pantry update via GitHub Actions workflow

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 /**
  * Constants used throughout the ts-pkgx package
  * This file is auto-generated from pantry. Do not edit manually.
- * Generated on 2025-11-15T02:34:38.528Z
+ * Generated on 2025-11-18T02:36:39.889Z
  */
 
 /**
