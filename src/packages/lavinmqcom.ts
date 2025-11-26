d357863064c795606f732f30b90ff08fba1b82dd
chrisbbreuer <chrisbreuer93@gmail.com>
2025-11-26 15:10:09 +0000

    chore: update air, LLaMA.cpp, lavinmq
    Automated update via GitHub Actions workflow

--- src/packages/lavinmqcom.ts
+++ src/packages/lavinmqcom.ts
@@ -3,7 +3,7 @@
  *
  * @domain `lavinmq.com`
  * @programs `lavinmq`, `lavinmqctl`, `lavinmqperf`
- * @version `2.5.4` (18 versions available)
+ * @version `2.5.5` (19 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install lavinmq.com`
@@ -19,7 +19,7 @@
  * console.log(pkg.name)        // "lavinmq"
  * console.log(pkg.description) // "Lightweight and fast AMQP (0-9-1) server"
  * console.log(pkg.programs)    // ["lavinmq", "lavinmqctl", ...]
- * console.log(pkg.versions[0]) // "2.5.4" (latest)
+ * console.log(pkg.versions[0]) // "2.5.5" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/lavinmq-com.md
@@ -83,6 +83,7 @@ export const lavinmqcomPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '2.5.5',
     '2.5.4',
     '2.5.3',
     '2.5.2',
