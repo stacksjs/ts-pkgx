29fc74048f3271379bb9736a0a75f2cdcd9d790c
chrisbbreuer <chrisbreuer93@gmail.com>
2025-11-30 15:10:35 +0000

    chore: update LLaMA.cpp, vim
    Automated update via GitHub Actions workflow

--- src/packages/github.com/ggerganov/llama.cpp.ts
+++ src/packages/github.com/ggerganov/llama.cpp.ts
@@ -3,7 +3,7 @@
  *
  * @domain `github.com/ggerganov/llama.cpp`
  * @programs `llama-cli`, `llama.cpp`, `convert.py`
- * @version `7206.0.0` (3811 versions available)
+ * @version `7207.0.0` (3812 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install github.com/ggerganov/llama.cpp`
@@ -18,7 +18,7 @@
  * console.log(pkg.name)        // "LLaMA.cpp"
  * console.log(pkg.description) // "LLM inference in C/C++"
  * console.log(pkg.programs)    // ["llama-cli", "llama.cpp", ...]
- * console.log(pkg.versions[0]) // "7206.0.0" (latest)
+ * console.log(pkg.versions[0]) // "7207.0.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/github-com/ggerganov/llama-cpp.md
@@ -83,6 +83,7 @@ export const llamacppPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '7207.0.0',
     '7206.0.0',
     '7205.0.0',
     '7204.0.0',
--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.1934` (2484 versions available)
+ * @version `9.1.1936` (2486 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -25,7 +25,7 @@
  * console.log(pkg.name)        // "vim"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.1934" (latest)
+ * console.log(pkg.versions[0]) // "9.1.1936" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -86,6 +86,8 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.1936',
+    '9.1.1935',
     '9.1.1934',
     '9.1.1933',
     '9.1.1932',
