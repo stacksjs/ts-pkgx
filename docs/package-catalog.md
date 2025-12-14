f69ca21fba8410a51ec9400cdd51e5a9cc723e40
chrisbbreuer <chrisbreuer93@gmail.com>
2025-12-14 03:39:35 +0000

    chore: update cargo-binstall, libcython, cython
    Automated update via GitHub Actions workflow

--- docs/package-catalog.md
+++ docs/package-catalog.md
@@ -8,7 +8,7 @@ Each package can be accessed using `getPackage(name)` or directly via `pantry.do
 
 - **Total Packages**: 1399
 - **Categories**: 27
-- **Last Updated**: 2025-12-14T02:48:04.347Z
+- **Last Updated**: 2025-12-14T03:39:34.067Z
 
 ## Table of Contents
 
--- src/packages/cythonorg.ts
+++ src/packages/cythonorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `cython.org`
  * @programs `cython`
- * @version `3.2.2` (28 versions available)
+ * @version `3.2.3` (29 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install cython.org`
@@ -19,7 +19,7 @@
  * console.log(pkg.name)        // "cython"
  * console.log(pkg.description) // "Compiler for writing C extensions for the Pytho..."
  * console.log(pkg.programs)    // ["cython"]
- * console.log(pkg.versions[0]) // "3.2.2" (latest)
+ * console.log(pkg.versions[0]) // "3.2.3" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/cython-org.md
@@ -75,6 +75,7 @@ export const cythonorgPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '3.2.3',
     '3.2.2',
     '3.2.1',
     '3.2.0',
