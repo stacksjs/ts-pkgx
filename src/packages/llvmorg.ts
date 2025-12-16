f6f7b7b06b1bc5b33cd686182bf70784f3705967
chrisbbreuer <chrisbreuer93@gmail.com>
2025-12-16 17:15:17 +0000

    chore: update LLaMA.cpp, llvm, steampipe, traefik
    Automated update via GitHub Actions workflow

--- src/packages/llvmorg.ts
+++ src/packages/llvmorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `llvm.org`
  * @programs `lld`, `clang`, `clang++`, `cc`, `c++`, ... (+9 more)
- * @version `21.1.7` (52 versions available)
+ * @version `21.1.8` (53 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install llvm.org`
@@ -19,7 +19,7 @@
  * console.log(pkg.name)        // "llvm"
  * console.log(pkg.description) // "The LLVM Project is a collection of modular and..."
  * console.log(pkg.programs)    // ["lld", "clang", ...]
- * console.log(pkg.versions[0]) // "21.1.7" (latest)
+ * console.log(pkg.versions[0]) // "21.1.8" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/llvm-org.md
@@ -91,6 +91,7 @@ export const llvmorgPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '21.1.8',
     '21.1.7',
     '21.1.6',
     '21.1.5',
