81b5bb31b4b2e7a2382a64ee230c5269bf724474
chrisbbreuer <chrisbreuer93@gmail.com>
2025-12-23 21:25:05 +0000

    chore: update aws/cli, psutil, gitlab-runner, vim
    Automated update via GitHub Actions workflow

--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.2011` (2557 versions available)
+ * @version `9.1.2017` (2563 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -25,7 +25,7 @@
  * console.log(pkg.name)        // "vim"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.2011" (latest)
+ * console.log(pkg.versions[0]) // "9.1.2017" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -86,6 +86,12 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.2017',
+    '9.1.2016',
+    '9.1.2015',
+    '9.1.2014',
+    '9.1.2013',
+    '9.1.2012',
     '9.1.2011',
     '9.1.2010',
     '9.1.2009',
