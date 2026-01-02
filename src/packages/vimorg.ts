9630978d8054be778d16f10e95e00380169fa7f3
chrisbbreuer <chrisbreuer93@gmail.com>
2026-01-02 15:25:33 +0000

    chore: update casdoor, vim
    Automated update via GitHub Actions workflow

--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.2042` (2585 versions available)
+ * @version `9.1.2045` (2588 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -25,7 +25,7 @@
  * console.log(pkg.name)        // "vim"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.2042" (latest)
+ * console.log(pkg.versions[0]) // "9.1.2045" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -86,6 +86,9 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.2045',
+    '9.1.2044',
+    '9.1.2043',
     '9.1.2042',
     '9.1.2041',
     '9.1.2040',
