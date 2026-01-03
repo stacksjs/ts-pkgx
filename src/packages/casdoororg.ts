096f36347f470bf52c1658c9ee75416762be3d9a
chrisbbreuer <chrisbreuer93@gmail.com>
2026-01-03 05:25:00 +0000

    chore: update casdoor
    Automated update via GitHub Actions workflow

--- src/packages/casdoororg.ts
+++ src/packages/casdoororg.ts
@@ -2,7 +2,7 @@
  * **casdoor** - pkgx package
  *
  * @domain `casdoor.org`
- * @version `2.217.0` (61 versions available)
+ * @version `2.218.0` (62 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install casdoor.org`
@@ -14,7 +14,7 @@
  *
  * const pkg = pantry.casdoororg
  * console.log(pkg.name)        // "casdoor"
- * console.log(pkg.versions[0]) // "2.217.0" (latest)
+ * console.log(pkg.versions[0]) // "2.218.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/casdoor-org.md
@@ -60,6 +60,7 @@ export const casdoororgPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '2.218.0',
     '2.217.0',
     '2.216.0',
     '2.215.0',
