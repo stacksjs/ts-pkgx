c33e0a24a559dd43d77fdf52a1fdccc0fa51b557
chrisbbreuer <chrisbreuer93@gmail.com>
2026-01-12 19:12:01 +0000

    chore: update crush, Vulkan-Loader
    Automated update via GitHub Actions workflow

--- docs/package-catalog.md
+++ docs/package-catalog.md
@@ -8,7 +8,7 @@ Each package can be accessed using `getPackage(name)` or directly via `pantry.do
 
 - **Total Packages**: 1399
 - **Categories**: 27
-- **Last Updated**: 2026-01-12T18:14:52.599Z
+- **Last Updated**: 2026-01-12T19:12:00.042Z
 
 ## Table of Contents
 
--- src/packages/charm.sh/crush.ts
+++ src/packages/charm.sh/crush.ts
@@ -2,7 +2,7 @@
  * **crush** - pkgx package
  *
  * @domain `charm.sh/crush`
- * @version `0.31.0` (12 versions available)
+ * @version `0.32.0` (13 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install charm.sh/crush`
@@ -14,7 +14,7 @@
  *
  * const pkg = pantry.charmshcrush
  * console.log(pkg.name)        // "crush"
- * console.log(pkg.versions[0]) // "0.31.0" (latest)
+ * console.log(pkg.versions[0]) // "0.32.0" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/charm-sh/crush.md
@@ -58,6 +58,7 @@ export const charmshcrushPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '0.32.0',
     '0.31.0',
     '0.30.3',
     '0.30.2',
