3a487745b461806772b767f7d1ad102bf0a5f8f2
chrisbbreuer <chrisbreuer93@gmail.com>
2026-01-17 11:12:18 +0000

    chore: update oauth2-proxy, vim
    Automated update via GitHub Actions workflow

--- src/packages/vimorg.ts
+++ src/packages/vimorg.ts
@@ -3,7 +3,7 @@
  *
  * @domain `vim.org`
  * @programs `vim`, `vi`
- * @version `9.1.2089` (2629 versions available)
+ * @version `9.1.2090` (2630 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install vim.org`
@@ -25,7 +25,7 @@
  * console.log(pkg.name)        // "vim"
  * console.log(pkg.description) // "The official Vim repository"
  * console.log(pkg.programs)    // ["vim", "vi"]
- * console.log(pkg.versions[0]) // "9.1.2089" (latest)
+ * console.log(pkg.versions[0]) // "9.1.2090" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/vim-org.md
@@ -86,6 +86,7 @@ export const vimPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '9.1.2090',
     '9.1.2089',
     '9.1.2088',
     '9.1.2087',
