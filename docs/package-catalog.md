9298947810feb99c6943e5222a4a5b283a5eddd2
chrisbbreuer <chrisbreuer93@gmail.com>
2026-01-23 01:38:52 +0000

    chore: update digger, Vulkan-Loader, zarf
    Automated update via GitHub Actions workflow

--- docs/package-catalog.md
+++ docs/package-catalog.md
@@ -8,7 +8,7 @@ Each package can be accessed using `getPackage(name)` or directly via `pantry.do
 
 - **Total Packages**: 1399
 - **Categories**: 27
-- **Last Updated**: 2026-01-23T00:29:13.823Z
+- **Last Updated**: 2026-01-23T01:38:50.513Z
 
 ## Table of Contents
 
--- src/packages/github.com/KhronosGroup/Vulkan-Loader.ts
+++ src/packages/github.com/KhronosGroup/Vulkan-Loader.ts
@@ -2,7 +2,7 @@
  * **Vulkan-Loader** - Vulkan Loader
  *
  * @domain `github.com/KhronosGroup/Vulkan-Loader`
- * @version `1.4.339` (77 versions available)
+ * @version `1.4.340` (78 versions available)
  * @versions From newest version to oldest.
  *
  * @install `launchpad install github.com/KhronosGroup/Vulkan-Loader`
@@ -17,7 +17,7 @@
  * const pkg = pantry.githubcomkhronosgroupvulkanloader
  * console.log(pkg.name)        // "Vulkan-Loader"
  * console.log(pkg.description) // "Vulkan Loader"
- * console.log(pkg.versions[0]) // "1.4.339" (latest)
+ * console.log(pkg.versions[0]) // "1.4.340" (latest)
  * ```
  *
  * @see https://ts-pkgx.netlify.app/packages/github-com/KhronosGroup/Vulkan-Loader.md
@@ -75,6 +75,7 @@ export const vulkanloaderPackage = {
    * @see https://ts-pkgx.netlify.app/usage for installation instructions
    */
   versions: [
+    '1.4.340',
     '1.4.339',
     '1.4.338',
     '1.4.337',
